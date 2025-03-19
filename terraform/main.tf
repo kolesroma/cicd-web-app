0225f8208dc39c935b135a5cedbd2f7986d3c416
Roma Kolesnyk <romakoles1@ukr.net>
2025-03-19 21:57:11 +0200

    add tf

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = (terraform.env("AWS_ACCESS_KEY_ID"))
+  secret_key = (terraform.env("AWS_SECRET_ACCESS_KEY"))
+}
+
+resource "aws_security_group" "Ec2SecurityGroup" {
+  name        = "Ec2SecurityGroup"
+  description = "Allow inbound traffic to EC2 instance"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "Ec2MainApplication" {
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = "t2.micro"
+  key_name      = var.key_name 
+  security_groups = [aws_security_group.Ec2SecurityGroup.name]
+  
+  tags = {
+    Name = "Ec2MainApplication"
+  }
+}
+
