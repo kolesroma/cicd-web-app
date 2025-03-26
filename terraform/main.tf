9e6ee2e840059a342162788914eea17427f491fd
Roma Kolesnyk <romakoles1@ukr.net>
2025-03-26 19:30:11 +0200

    ref4

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,32 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_security_group" "Ec2SecurityGroup" {
+  name        = "Ec2SecurityGroup"
+  description = "Allow inbound traffic to EC2 instance"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "Ec2MainApplication" {
   ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
